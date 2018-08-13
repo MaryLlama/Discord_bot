4d47ff3713794efa40e25a9292bac3dd8e673cc8
MaryLlama <42252331+MaryLlama@users.noreply.github.com>
2018-08-13 16:51:30 -0400

    New Token

--- Ref.java
+++ Ref.java
@@ -1,6 +1,6 @@
 package com.discord.Anthem;
 
 public class Ref {
-	public static final String token  = "NDc3MDE3NDc0ODY1NDMwNTI5.Dk5npA.oSm9IH7IoVpYy9v5asEMj6r8rTQ";
+	public static final String token  = "NDc4MzcyNTQ1OTUxODI1OTIw.DlJxZg.3dpDcXIius9qYAG8X4q1syac1VM";
 	public static final String prefix  = "h!";
 }
